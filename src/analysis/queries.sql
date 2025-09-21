caba7bb044399126226fe104b6248ced2c136ead
JHalt06 <jamesgeorgehalt@gmail.com>
2025-09-20 22:18:29 -0400

    good progress, next work on making it resusable for any ticker and loop it.

--- src/analysis/queries.sql
+++ src/analysis/queries.sql
@@ -1,4 +1,5 @@
 --This direcotry is where SQL queires and analysis scripts
 --Read from the database, not write to it.
-
 --Add a query that shows something cool, like top gainers in last 24 hours, 52 week highs, etc.
+
+SELECT MAX(timestamp) FROM day_prices WHERE ticker = ?
--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,3 +1,13 @@
 --This directory holds SQL schema files and scripts for migration
-
 --stock and stock_prices tables
+
+CREATE TABLE IF NOT EXISTS stock_prices(
+    PRIMARY KEY (symbol, timestamp)
+    symbol TEXT NOT NULL,
+    timestamp TIMESTAMPTZ NOT NULL,
+    open
+    high
+    low
+    close
+    volume 
+)
